60065f4e7139f202db32f05ca34049597e2bf180
Colin Skinner <colinaskinner@gmail.com>
2026-08-24 22:26:11 -0700

    last before merge

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -9,11 +9,6 @@ export default defineConfig({
           target: 'http://127.0.0.1:5001',
           changeOrigin: true,
         },
-        '/api/telemetry': {
-          target: 'http://127.0.0.1:5701',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/telemetry/, ''),
-        },
       },
     },
   },
